d6d098d1efa2771a34b29e40556ebc3d8acdc034
김동진 <ladmusician.kim@gmail.com>
2015-07-14 21:55:22 +0900

    implement codeigniter-restserver & migration mysql

---
+++ application/models/Menu_model.php
@@ -0,0 +1,16 @@
+<?php
+class Menu_model extends CI_Model {
+    function __construct()
+    {    	
+        parent::__construct();
+    }
+    function gets(){
+    	return $this->db->query("SELECT * FROM lunch")->result();
+    }
+    function get($menu_id){
+    	return $this->db->get_where('lunch', array('id'=>$menu_id))->row();
+    }
+    function countAll() {
+        return $this->db->count_all_result('lunch');
+    }
+}
