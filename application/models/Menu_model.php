f48d661b09980fd0df35bcf6493e6d71a68bebba
김동진 <ladmusician.kim@gmail.com>
2015-07-14 22:09:38 +0900

    change query in menu_model

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -8,7 +8,7 @@ class Menu_model extends CI_Model {
     	return $this->db->query("SELECT * FROM lunch")->result();
     }
     function get($menu_id){
-    	return $this->db->get_where('lunch', array('id'=>$menu_id))->row();
+    	return $this->db->get_where('lunch', array('_id'=>$menu_id))->row();
     }
     function countAll() {
         return $this->db->count_all_result('lunch');
